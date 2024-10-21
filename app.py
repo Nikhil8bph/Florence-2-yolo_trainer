83ef0fb2d1b701a6fa670287e48869f91dbeff90
Nikhil Sharma <nikhil8.bph@gmail.com>
2024-10-21 12:36:24 +0530

    project change and multiple project state verification rules

--- app.py
+++ app.py
@@ -21,23 +21,28 @@ app.config['UPLOAD_FOLDER'] = data_path
 @app.route('/upload_files', methods=['POST'])
 def upload_files():
     current_state = utils.read_current_state()
-    if 'files[]' not in request.files:
-        return jsonify({"message": "No files part in the request"}), 400
+    if(current_state["project_created"]==True):
+        if 'files[]' not in request.files:
+            return jsonify({"message": "No files part in the request"}), 400
 
-    files = request.files.getlist('files[]')
-    uploaded_files = []
-    for file in files:
-        if file.filename == '':
-            return jsonify({"message": "No selected file"}), 400
+        files = request.files.getlist('files[]')
+        uploaded_files = []
+        for file in files:
+            if file.filename == '':
+                return jsonify({"message": "No selected file"}), 400
 
-        # Save the file to the specified upload folder
-        project_path = os.path.join(app.config['UPLOAD_FOLDER'],current_state['current_project'])
-        project_path_images = os.path.join(project_path,"images")
-        os.makedirs(project_path_images, exist_ok=True)
-        file_path = os.path.join(project_path_images, file.filename)
-        file.save(file_path)
-        uploaded_files.append(file.filename)
-    return jsonify({"message": "Files uploaded successfully", "files": uploaded_files}), 200
+            # Save the file to the specified upload folder
+            project_path = os.path.join(app.config['UPLOAD_FOLDER'],current_state['current_project'])
+            project_path_images = os.path.join(project_path,"images")
+            os.makedirs(project_path_images, exist_ok=True)
+            file_path = os.path.join(project_path_images, file.filename)
+            file.save(file_path)
+            uploaded_files.append(file.filename)
+        current_state['image_upload']=True
+        utils.update_current_state(current_state)
+        return jsonify({"message": "Files uploaded successfully", "files": uploaded_files}), 200
+    else:
+        return "project not created"
 
 @app.route('/create_new_project',methods=['POST'])
 def create_project():
@@ -55,16 +60,26 @@ def list_projects():
     else:
         return "Only POST method is allowed"
     
+@app.route('/change_current_project',methods=['POST'])
+def change_current_project():
+    if request.method == 'POST':
+        project_name = request.form["project_name"]
+        utils.change_current_state(data_path,project_name)
+        return utils.read_current_state()['current_project']
+    else:
+        return "Only GET method is allowed"
+    
 @app.route('/get_current_project',methods=['GET'])
 def get_current_project():
     if request.method == 'GET':
-        return utils.read_current_state()
+        return utils.read_current_state()['current_project']
     else:
         return "Only GET method is allowed"
 
 @app.route('/start_annotation',methods=["POST"])
 def start_annotation():
-    if request.method == 'POST':
+    current_state = utils.read_current_state()
+    if request.method == 'POST' and current_state["image_upload"]==True :
         task_perform = None
         prompt = request.form["task_prompt"]
         task_type = int(request.form["task_type"])
@@ -77,13 +92,16 @@ def start_annotation():
             processor = AutoProcessor.from_pretrained(model_id, trust_remote_code=True)
             for progress in run_annotation_tool(data_path, task_perform, prompt, model, processor):
                 yield f"data: {progress}\n\n"
+        current_state['image_annotation']=True
+        utils.update_current_state(current_state)
         return Response(task(task_perform,prompt), mimetype='text/event-stream')
     else:
         return "Only POST method is allowed"
 
 @app.route('/start_model_training',methods=["POST"])
 def start_model_training():
-    if request.method == 'POST':
+    current_state = utils.read_current_state()
+    if request.method == 'POST' and current_state["model_training"]==True:
         yolo_model_version = request.form["yolo_model_version"]
         yolo_model_epochs = int(request.form["yolo_model_epochs"])
         image_size = int(request.form["image_size"])
@@ -99,9 +117,13 @@ def start_model_training():
                 yield result
         thread1 = threading.Thread(target=task1)
         thread1.start()
+        current_state['model_training']=True
+        utils.update_current_state(current_state)
         return Response(task2(thread=thread1), mimetype='text/event-stream')
     else:
         return "Only POST method is allowed"
+    
+
 
 if __name__ == '__main__':
     app.run()
--- utils.py
+++ utils.py
@@ -4,7 +4,12 @@ import glob
 
 def create_project(data_path, project_name):
     base_path = "current_state.json"
-    current_project = {"current_project": project_name,"data_path":data_path,"project_path":os.path.join(data_path, project_name)}
+    current_project = {"current_project": project_name,"data_path":data_path,
+                       "project_path":os.path.join(data_path, project_name),
+                       "project_created":True,
+                       "image_upload":False,
+                       "image_annotation":False,
+                       "model_training":False}
     try:
         with open(base_path, 'w') as file:
             json.dump(current_project, file)
@@ -12,6 +17,14 @@ def create_project(data_path, project_name):
         print(f"Error writing to file {base_path}: {e}")
     os.makedirs(os.path.join(data_path, project_name), exist_ok=True)
     os.makedirs(os.path.join(data_path, project_name, "images"), exist_ok=True)
+
+def update_project_state(current_project_state):
+    base_path = "current_state.json"
+    try:
+        with open(base_path, 'w') as file:
+            json.dump(current_project_state, file)
+    except IOError as e:
+        print(f"Error writing to file {base_path}: {e}")
 
 def read_current_state():
     try:
@@ -40,3 +53,29 @@ def get_last_created_folder(directory):
 
 def provide_list_of_projects(data_path):
     return os.listdir(data_path)
+
+def change_current_state(base_path,project_name):
+    current_state_path = "current_state.json"
+    current_project = {"current_project": None,"data_path":None,
+                       "project_path":None,
+                       "project_created":False,
+                       "image_upload":False,
+                       "image_annotation":False,
+                       "model_training":False}
+    if project_name in os.listdir(base_path):
+        current_project["current_project"] = project_name
+        current_project["data_path"] = base_path
+        current_project["project_path"] = os.path.join(base_path, project_name)
+        current_project["project_created"] = True
+        if len(os.path.join(current_project["project_path"],"images")) > 0:
+            current_project["image_upload"] = True
+        if len(os.path.join(current_project["project_path"],"labels")) > 0:
+            current_project["image_annotation"] = True
+        if len(os.path.join(current_project["project_path"],"runs")) > 3:
+            current_project["model_training"] = True
+        try:
+            with open(current_state_path, 'w') as file:
+                json.dump(current_project, file)
+        except IOError as e:
+            print(f"Error writing to file {current_state_path}: {e}") 
+        
